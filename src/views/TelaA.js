df9ea616df1d5be482fa5d8a3a73d53383ec144e
Maro de Melo <marodemelo10@gmail.com>
2021-04-21 16:26:46 -0300

    Studying react-native navigation system using stacks

---
+++ src/views/TelaA.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import TextoCentral from '../components/TextoCentral'
+
+export default props => (
+    <TextoCentral corFundo='#A00'>
+        Tela A
+    </TextoCentral>
+)
