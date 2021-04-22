67f07969c25853ecdd318bb91048b2bc4183f747
Maro de Melo <marodemelo10@gmail.com>
2021-04-22 19:22:00 -0300

    Tentando corrigir o bug no stack navigation

--- src/views/TelaA.js
+++ src/views/TelaA.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import TextoCentral from '../components/TextoCentral'
+import PassoStack from '../components/PassoStack'
 
-export default props => (
-    <TextoCentral corFundo='#A00'>
-        Tela A
-    </TextoCentral>
+export default props => 
+    /* console.warn(props.navigation != null)
+    console.warn(props.navigation.navigate != null) */
+   
+    (
+    
+        <TextoCentral corFundo='#A00'>
+            Tela A
+        </TextoCentral>
+    
 )
