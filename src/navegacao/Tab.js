87042e56d2ef14cdf5eccb377854cd0222b68697
Maro de Melo <marodemelo10@gmail.com>
2021-04-24 10:10:04 -0300

    Utilizando ícones para realizar a navegacao com tabs

--- src/navegacao/Tab.js
+++ src/navegacao/Tab.js
@@ -3,14 +3,41 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import TelaA from '../views/TelaA'
 import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 const Tab = createBottomTabNavigator()
 
 export default props => (
-    <Tab.Navigator tabBarOptions={{activeTintColor: 'red', inactiveTintColor: 'blue', labelStyle: {fontSize: 30}}}initialRouteName="TelaB">
-        <Tab.Screen name='TelaA' component={TelaA}/>
-        <Tab.Screen name='TelaB' component={TelaB}/>
-        <Tab.Screen name='TelaC' component={TelaC}/>
+    <Tab.Navigator 
+    screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+            switch (route.name) {
+                case 'TelaA':
+                    iconName = focused
+                        ? 'ios-information-circle'
+                        : 'ios-information-circle-outline';
+                    break;
+
+                    case 'TelaB':
+                        iconName = focused
+                            ? 'ios-information-circle'
+                            : 'ios-information-circle-outline';
+                        break;
+                    
+                        case 'TelaC':
+                            iconName = focused ? 'ios-list-box' : 'ios-list';
+                            break;          
+            }         
+          
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+     
+    tabBarOptions={{showLabel:true, activeTintColor: 'red', inactiveTintColor: 'blue',}} initialRouteName="TelaB" >
+        <Tab.Screen name='TelaA' component={TelaA} options={{title:'Inicial'}}/>
+        <Tab.Screen name='TelaB' component={TelaB} options={{title:'Meio'}}/>
+        <Tab.Screen name='TelaC' component={TelaC} options={{title:'Final'}}/>
     </Tab.Navigator>
 )
