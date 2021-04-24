45d3f6599d11b6aa826b1020b4992e87a8400fa8
Maro de Melo <marodemelo10@gmail.com>
2021-04-24 08:09:01 -0300

    Estudando o método navigation.push

--- src/components/PassoStack.js
+++ src/components/PassoStack.js
@@ -3,8 +3,9 @@ import {View, Button} from 'react-native'
 
 export default props => (
     <View style={{flex:1}}>
-        <View>
-            {props.avancar ? <Button title='Avançar' onPress={()=> { props.navigation.navigate(props.avancar)}} /> : false}
+        <View style={{flexDirection:'row',justifyContent: 'space-around'}}>           
+            {props.voltar ? <Button title='Voltar' onPress={()=> { props.navigation.goBack()}} /> : false}
+            {props.avancar ? <Button title='Avançar' onPress={()=> { props.navigation.push(props.avancar)}} /> : false}
         </View>
         <View style={{flex:1}}>
             {props.children}
