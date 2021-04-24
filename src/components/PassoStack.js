bc0208171e63c874cfc691e68d134c1f71c98c0e
Maro de Melo <marodemelo10@gmail.com>
2021-04-24 08:44:36 -0300

    Estudando navegacao com bottom tabs

--- src/components/PassoStack.js
+++ src/components/PassoStack.js
@@ -5,7 +5,7 @@ export default props => (
     <View style={{flex:1}}>
         <View style={{flexDirection:'row',justifyContent: 'space-around'}}>           
             {props.voltar ? <Button title='Voltar' onPress={()=> { props.navigation.goBack()}} /> : false}
-            {props.avancar ? <Button title='Avançar' onPress={()=> { props.navigation.push(props.avancar, props.avancarParams )}} /> : false}
+            {props.avancar ? <Button title='Avançar' onPress={()=> { props.navigation.navigate(props.avancar, props.avancarParams )}} /> : false}
         </View>
         <View style={{flex:1}}>
             {props.children}
--- src/views/TelaC.js
+++ src/views/TelaC.js
@@ -2,11 +2,11 @@ import React from 'react'
 import TextoCentral from '../components/TextoCentral'
 
 export default props => { 
-    const route = props.route || {params: { numero: 0}} 
+    const numero = props.route && props.route.params && props.route.params.numero ? props.route.params.numero : 0 
     console.warn(props.route.params)
 return (
     <TextoCentral corFundo='#9932cd'>
-        Tela C = {route.params.numero}
+        Tela C = {numero}
     </TextoCentral>
 )
 }
